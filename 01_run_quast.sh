33fbe44ee041bfab57485b1b37a0ce8a2be85f97
Israel Shade Niece <lwy647@centaur.ag.utk.edu>
2025-08-04 11:05:19 -0400

    Initial commit of draft assembly project

---
+++ 01_run_quast.sh
@@ -0,0 +1,3 @@
+quast.py \
+   -t 10 \
+   discula70_53.scafSeq.fasta
---
+++ 02_run_busco.sh
@@ -0,0 +1,6 @@
+busco \
+        -i discula70_53.scafSeq \
+        -m genome \
+        -l sordariomycetes_odb10 \
+        -c 15 \
+        --out discula70_53.scafSeq.BUSCO
